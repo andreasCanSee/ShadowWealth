682fb4768105e50e298e300301111d4ef191119d
andreasCanSee <andreas.kansy@gmail.com>
2024-01-30 12:25:02 +0100

    Added Header Component with Title

---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<header class="bg-gradient-to-r from-purple-800 to-blue-400 text-yellow-100 p-4 text-center flex flex-col justify-center">
+    <h1 class="text-3xl font-bold mb-2">ShadowWealth</h1>
+    <p class="text-xl">Entdecke deine versteckten Sparpotentiale</p>
+</header>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<div class="text-center p-10 bg-gray-100">
-    <h1 class="text-4xl font-bold text-blue-600">Welcome to SvelteKit</h1>
-    <p class="mt-3 text-lg text-gray-700">Visit <a href="https://kit.svelte.dev" class="text-blue-500 hover:text-blue-800 underline">kit.svelte.dev</a> to read the documentation</p>
-  </div>
+<script lang="ts">
+    import Header from "../components/Header.svelte";
+</script>
+
+<div class="min-h-screen bg-yellow-100">
+    <Header />
+</div>
